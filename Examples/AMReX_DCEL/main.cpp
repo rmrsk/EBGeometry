10ec5ca599c8f67810b6a1b8ba5d00b686986acb
Robert Marskar <39047996+rmrsk@users.noreply.github.com>
2022-05-01 18:41:34 +0200

    Add clang code formatting (#27)

--- Examples/AMReX_DCEL/main.cpp
+++ Examples/AMReX_DCEL/main.cpp
@@ -16,92 +16,100 @@
 using namespace amrex;
 
 namespace amrex {
-  namespace EB2 {
+namespace EB2 {
 
-    /*!
-      @brief This is an AMReX-capable version of the EBGeometry BVH accelerator. It is templated as T, BV, K which indicate
-      the EBGeometry precision, bounding volume, and tree degree. 
-    */
-    template <class T, class BV, int K>
-    class SignedDistanceBVH {
-    public:
+/*!
+  @brief This is an AMReX-capable version of the EBGeometry BVH accelerator. It
+  is templated as T, BV, K which indicate the EBGeometry precision, bounding
+  volume, and tree degree.
+*/
+template <class T, class BV, int K>
+class SignedDistanceBVH
+{
+public:
+  /*!
+@brief Alias for builder node, for encapsulating a "standard" BVH node
+  */
+  using BuilderNode = EBGeometry::BVH::NodeT<T, EBGeometry::DCEL::FaceT<T>, BV, K>;
 
-      /*!
-	@brief Alias for builder node, for encapsulating a "standard" BVH node
-      */
-      using BuilderNode = EBGeometry::BVH::NodeT<T, EBGeometry::Dcel::FaceT<T>, BV, K>;
+  /*!
+@brief Alias for linearized BVH node
+  */
+  using LinearNode = EBGeometry::BVH::LinearBVH<T, EBGeometry::DCEL::FaceT<T>, BV, K>;
 
-      /*!
-	@brief Alias for linearized BVH node
-      */
-      using LinearNode = EBGeometry::BVH::LinearBVH<T, EBGeometry::Dcel::FaceT<T>, BV, K>;
+  /*!
+@brief Alias for always-3D vector
+  */
+  using Vec3 = EBGeometry::Vec3T<T>;
 
-      /*!
-	@brief Alias for always-3D vector
-      */
-      using Vec3 = EBGeometry::Vec3T<T>;
+  /*!
+@brief Full constructor.
+@param[in] a_filename File name. Must be a PLY file and will be parser by the
+PLY parser.
+@param[in] a_flipSign Hook for swapping inside/outside.
+  */
+  SignedDistanceBVH(const std::string a_filename, const bool a_flipSign)
+  {
 
-      /*!
-	@brief Full constructor.
-	@param[in] a_filename File name. Must be a PLY file and will be parser by the PLY parser.
-	@param[in] a_flipSign Hook for swapping inside/outside. 
-      */
-      SignedDistanceBVH(const std::string a_filename, const bool a_flipSign) {
+    // 1. Read mesh from file.
+    auto mesh = EBGeometry::Parser::PLY<T>::readIntoDCEL(a_filename);
 
-	// 1. Read mesh from file. 
-	auto mesh = EBGeometry::Dcel::Parser::PLY<T>::readASCII(a_filename);
+    // 2. Create a standard BVH hierarchy. This is not a compact ree.
+    auto root = std::make_shared<BuilderNode>(mesh->getFaces());
+    root->topDownSortAndPartitionPrimitives(
+      EBGeometry::DCEL::defaultBVConstructor<T, BV>, EBGeometry::DCEL::defaultPartitioner<T, BV, K>,
+      EBGeometry::DCEL::defaultStopFunction<T, BV, K>);
 
-	// 2. Create a standard BVH hierarchy. This is not a compact ree. 
-	auto root = std::make_shared<BuilderNode>(mesh->getFaces());
-	root->topDownSortAndPartitionPrimitives(EBGeometry::Dcel::defaultBVConstructor<T, BV>,
-						EBGeometry::Dcel::defaultPartitioner<T, BV, K>,
-						EBGeometry::Dcel::defaultStopFunction<T, BV, K>);
-
-	// 3. Flatten the tree onto a tighter memory representation. 
-	m_rootNode = root->flattenTree();
-      }
-
-      /*!
-	@brief Copy constructor. 
-	@param[in] a_other Other SDF. 
-      */
-      SignedDistanceBVH(const SignedDistanceBVH& a_other) {
-	this->m_rootNode     = a_other.m_rootNode;
-	this->m_flipSign     = a_other.m_flipSign;
-      }
-
-      /*!
-	@brief AMReX's implicit function definition. 
-      */
-      Real operator() (AMREX_D_DECL(Real x, Real y, Real z)) const noexcept {
-	const Real sign = (m_flipSign) ? -1.0 : 1.0;
-
-	return sign * m_rootNode->signedDistance(Vec3(x,y,z));
-      };
-
-      /*!
-	@brief Also an AMReX implicit function implementation
-      */
-      inline Real operator() (const RealArray& p) const noexcept {
-        return this->operator()(AMREX_D_DECL(p[0],p[1],p[2]));
-      }
-
-    protected:
-
-      /*!
-	@brief Root node of the linearized BVH hierarchy. 
-      */
-      std::shared_ptr<LinearNode> m_rootNode;
-
-      /*!
-	@brief Hook for flipping the sign
-      */
-      bool m_flipSign;      
-    };
+    // 3. Flatten the tree onto a tighter memory representation.
+    m_rootNode = root->flattenTree();
   }
-}
 
-int main (int argc, char* argv[]) {
+  /*!
+@brief Copy constructor.
+@param[in] a_other Other SDF.
+  */
+  SignedDistanceBVH(const SignedDistanceBVH& a_other)
+  {
+    this->m_rootNode = a_other.m_rootNode;
+    this->m_flipSign = a_other.m_flipSign;
+  }
+
+  /*!
+@brief AMReX's implicit function definition.
+  */
+  Real operator()(AMREX_D_DECL(Real x, Real y, Real z)) const noexcept
+  {
+    const Real sign = (m_flipSign) ? -1.0 : 1.0;
+
+    return sign * m_rootNode->signedDistance(Vec3(x, y, z));
+  };
+
+  /*!
+@brief Also an AMReX implicit function implementation
+  */
+  inline Real
+  operator()(const RealArray& p) const noexcept
+  {
+    return this->operator()(AMREX_D_DECL(p[0], p[1], p[2]));
+  }
+
+protected:
+  /*!
+@brief Root node of the linearized BVH hierarchy.
+  */
+  std::shared_ptr<LinearNode> m_rootNode;
+
+  /*!
+@brief Hook for flipping the sign
+  */
+  bool m_flipSign;
+};
+} // namespace EB2
+} // namespace amrex
+
+int
+main(int argc, char* argv[])
+{
   amrex::Initialize(argc, argv);
 
   int n_cell = 128;
@@ -116,53 +124,47 @@ int main (int argc, char* argv[]) {
   pp.query("max_grid_size", max_grid_size);
   pp.query("which_geom", which_geom);
 
-	
   Geometry geom;
   {
     RealBox rb;
-	  
-    if(which_geom == 0){// Airfoil case
-      rb = RealBox({-100,-100,-75}, {400,100,125}); 
-      filename = "../PLY/airfoil.ply";
-    }
-    else if (which_geom == 1){ // Sphere case
-      rb = RealBox({-400,-400,-400}, {400,400,400});
-      filename = "../PLY/sphere.ply";
-    }
-    else if (which_geom == 2){ // Dodecahedron
-      rb = RealBox({-2.,-2.,-2.}, {2.,2.,2.});
-      filename = "../PLY/dodecahedron.ply";
-    }
-    else if (which_geom == 3){ // Horse
-      rb = RealBox({-0.12,-0.12,-0.12}, {0.12,0.12,0.12});
-      filename = "../PLY/horse.ply";
-    }
-    else if (which_geom == 4){ // Car
-      //	    rb = RealBox({-20,-20,-20}, {20,20,20}); // Doesn't work. 
-      rb = RealBox({-10,-5,-5}, {10,5,5}); // Works. 
-      filename = "../PLY/porsche.ply";
-    }
-    else if (which_geom == 5){ // Orion
-      rb = RealBox({-10,-5,-10}, {10,10,10}); 
-      filename = "../PLY/orion.ply";
-    }
-    else if (which_geom == 6){ // Armadillo
-      rb = RealBox({-100,-75,-100}, {100,125,100}); 
-      filename = "../PLY/armadillo.ply";
-    }	  	  	  	  
 
-    Array<int,AMREX_SPACEDIM> is_periodic{false, false, false};
+    if (which_geom == 0) { // Airfoil case
+      rb = RealBox({-100, -100, -75}, {400, 100, 125});
+      filename = "../PLY/airfoil.ply";
+    } else if (which_geom == 1) { // Sphere case
+      rb = RealBox({-400, -400, -400}, {400, 400, 400});
+      filename = "../PLY/sphere.ply";
+    } else if (which_geom == 2) { // Dodecahedron
+      rb = RealBox({-2., -2., -2.}, {2., 2., 2.});
+      filename = "../PLY/dodecahedron.ply";
+    } else if (which_geom == 3) { // Horse
+      rb = RealBox({-0.12, -0.12, -0.12}, {0.12, 0.12, 0.12});
+      filename = "../PLY/horse.ply";
+    } else if (which_geom == 4) { // Car
+      //	    rb = RealBox({-20,-20,-20}, {20,20,20}); // Doesn't work.
+      rb = RealBox({-10, -5, -5}, {10, 5, 5}); // Works.
+      filename = "../PLY/porsche.ply";
+    } else if (which_geom == 5) { // Orion
+      rb = RealBox({-10, -5, -10}, {10, 10, 10});
+      filename = "../PLY/orion.ply";
+    } else if (which_geom == 6) { // Armadillo
+      rb = RealBox({-100, -75, -100}, {100, 125, 100});
+      filename = "../PLY/armadillo.ply";
+    }
+
+    Array<int, AMREX_SPACEDIM> is_periodic{false, false, false};
     Geometry::Setup(&rb, 0, is_periodic.data());
-    Box domain(IntVect(0), IntVect(n_cell-1));
+    Box domain(IntVect(0), IntVect(n_cell - 1));
     geom.define(domain);
   }
 
-  // Create our signed distance function. K is the tree degree while T is the EBGeometry precision. 
+  // Create our signed distance function. K is the tree degree while T is the
+  // EBGeometry precision.
   constexpr int K = 4;
-	
-  using T    = float;
+
+  using T = float;
   using Vec3 = EBGeometry::Vec3T<T>;
-  using BV   = EBGeometry::BoundingVolumes::AABBT<T>;
+  using BV = EBGeometry::BoundingVolumes::AABBT<T>;
 
   EB2::SignedDistanceBVH<T, BV, K> sdf(filename, false);
 
@@ -172,14 +174,14 @@ int main (int argc, char* argv[]) {
   // Put some data
   MultiFab mf;
   {
-    BoxArray ba(geom.Domain());
-    ba.maxSize(max_grid_size);
-    DistributionMapping dm{ba};
+    BoxArray boxArray(geom.Domain());
+    boxArray.maxSize(max_grid_size);
+    DistributionMapping dm{boxArray};
 
-    std::unique_ptr<EBFArrayBoxFactory> factory
-      = amrex::makeEBFabFactory(geom, ba, dm, {2,2,2}, EBSupport::full);
+    std::unique_ptr<EBFArrayBoxFactory> factory =
+      amrex::makeEBFabFactory(geom, boxArray, dm, {2, 2, 2}, EBSupport::full);
 
-    mf.define(ba, dm, 1, 0, MFInfo(), *factory);
+    mf.define(boxArray, dm, 1, 0, MFInfo(), *factory);
     mf.setVal(1.0);
   }
 
--- Source/EBGeometry_VecImplem.hpp
+++ Source/EBGeometry_VecImplem.hpp
@@ -21,58 +21,64 @@
 #include "EBGeometry_NamespaceHeader.hpp"
 
 template <class T>
-inline
-Vec2T<T>::Vec2T(){
+inline Vec2T<T>::Vec2T()
+{
   *this = Vec2T<T>::zero();
 }
 
 template <class T>
-inline
-Vec2T<T>::Vec2T(const Vec2T& u) noexcept {
+inline Vec2T<T>::Vec2T(const Vec2T& u) noexcept
+{
   x = u.x;
   y = u.y;
 }
 
 template <class T>
-inline
-Vec2T<T>::Vec2T(const T& a_x, const T& a_y){
+inline Vec2T<T>::Vec2T(const T& a_x, const T& a_y)
+{
   x = a_x;
   y = a_y;
 }
 
 template <class T>
-inline
-constexpr Vec2T<T> Vec2T<T>::zero() noexcept {
+inline constexpr Vec2T<T>
+Vec2T<T>::zero() noexcept
+{
   return Vec2T<T>(T(0.0), T(0.0));
 }
 
 template <class T>
-inline
-constexpr Vec2T<T> Vec2T<T>::one() noexcept {
+inline constexpr Vec2T<T>
+Vec2T<T>::one() noexcept
+{
   return Vec2T<T>(T(1.0), T(1.0));
 }
 
 template <class T>
-inline
-constexpr Vec2T<T> Vec2T<T>::min() noexcept {
+inline constexpr Vec2T<T>
+Vec2T<T>::min() noexcept
+{
   return Vec2T<T>(-std::numeric_limits<T>::max(), -std::numeric_limits<T>::max());
 }
 
 template <class T>
-inline
-constexpr Vec2T<T> Vec2T<T>::max() noexcept {
+inline constexpr Vec2T<T>
+Vec2T<T>::max() noexcept
+{
   return Vec2T<T>(std::numeric_limits<T>::max(), std::numeric_limits<T>::max());
 }
 
 template <class T>
-inline
-constexpr Vec2T<T> Vec2T<T>::infinity() noexcept {
+inline constexpr Vec2T<T>
+Vec2T<T>::infinity() noexcept
+{
   return Vec2T<T>(std::numeric_limits<T>::infinity(), std::numeric_limits<T>::infinity());
 }
 
 template <class T>
-inline
-Vec2T<T>& Vec2T<T>::operator=(const Vec2T<T>& u) noexcept {
+inline Vec2T<T>&
+Vec2T<T>::operator=(const Vec2T<T>& u) noexcept
+{
   x = u.x;
   y = u.y;
 
@@ -80,39 +86,45 @@ Vec2T<T>& Vec2T<T>::operator=(const Vec2T<T>& u) noexcept {
 }
 
 template <class T>
-inline
-Vec2T<T> Vec2T<T>::operator+(const Vec2T<T>& u) const noexcept {
-  return Vec2T<T>(x+u.x, y+u.y);
+inline Vec2T<T>
+Vec2T<T>::operator+(const Vec2T<T>& u) const noexcept
+{
+  return Vec2T<T>(x + u.x, y + u.y);
 }
 
 template <class T>
-inline
-Vec2T<T> Vec2T<T>::operator-(const Vec2T<T>& u) const noexcept {
-  return Vec2T<T>(x-u.x, y-u.y);
+inline Vec2T<T>
+Vec2T<T>::operator-(const Vec2T<T>& u) const noexcept
+{
+  return Vec2T<T>(x - u.x, y - u.y);
 }
 
 template <class T>
-inline
-Vec2T<T> Vec2T<T>::operator-() const noexcept {
+inline Vec2T<T>
+Vec2T<T>::operator-() const noexcept
+{
   return Vec2T<T>(-x, -y);
 }
 
 template <class T>
-inline
-Vec2T<T> Vec2T<T>::operator*(const T& s) const noexcept {
-  return Vec2T<T>(x*s, y*s);
+inline Vec2T<T>
+Vec2T<T>::operator*(const T& s) const noexcept
+{
+  return Vec2T<T>(x * s, y * s);
 }
 
 template <class T>
-inline
-Vec2T<T> Vec2T<T>::operator/(const T& s) const noexcept {
-  const T is = 1./s;
-  return Vec2T<T>(x*is, y*is);
+inline Vec2T<T>
+Vec2T<T>::operator/(const T& s) const noexcept
+{
+  const T is = 1. / s;
+  return Vec2T<T>(x * is, y * is);
 }
 
 template <class T>
-inline
-Vec2T<T>& Vec2T<T>::operator+=(const Vec2T<T>& u) noexcept {
+inline Vec2T<T>&
+Vec2T<T>::operator+=(const Vec2T<T>& u) noexcept
+{
   x += u.x;
   y += u.y;
 
@@ -120,8 +132,9 @@ Vec2T<T>& Vec2T<T>::operator+=(const Vec2T<T>& u) noexcept {
 }
 
 template <class T>
-inline
-Vec2T<T>& Vec2T<T>::operator-=(const Vec2T<T>& u) noexcept {
+inline Vec2T<T>&
+Vec2T<T>::operator-=(const Vec2T<T>& u) noexcept
+{
   x -= u.x;
   y -= u.y;
 
@@ -129,8 +142,9 @@ Vec2T<T>& Vec2T<T>::operator-=(const Vec2T<T>& u) noexcept {
 }
 
 template <class T>
-inline
-Vec2T<T>& Vec2T<T>::operator*=(const T& s) noexcept {
+inline Vec2T<T>&
+Vec2T<T>::operator*=(const T& s) noexcept
+{
   x *= s;
   y *= s;
 
@@ -138,10 +152,11 @@ Vec2T<T>& Vec2T<T>::operator*=(const T& s) noexcept {
 }
 
 template <class T>
-inline
-Vec2T<T>& Vec2T<T>::operator/=(const T& s) noexcept {
-  const T is = 1./s;
-    
+inline Vec2T<T>&
+Vec2T<T>::operator/=(const T& s) noexcept
+{
+  const T is = 1. / s;
+
   x *= is;
   y *= is;
 
@@ -149,72 +164,80 @@ Vec2T<T>& Vec2T<T>::operator/=(const T& s) noexcept {
 }
 
 template <class T>
-inline
-T Vec2T<T>::dot(const Vec2T<T>& u) const noexcept {
-  return x*u.x + y*u.y;
+inline T
+Vec2T<T>::dot(const Vec2T<T>& u) const noexcept
+{
+  return x * u.x + y * u.y;
 }
 
 template <class T>
-inline
-T Vec2T<T>::length() const noexcept {
-  return sqrt(x*x + y*y);
+inline T
+Vec2T<T>::length() const noexcept
+{
+  return sqrt(x * x + y * y);
 }
 
 template <class T>
-inline
-T Vec2T<T>::length2() const noexcept {
-  return x*x + y*y;
+inline T
+Vec2T<T>::length2() const noexcept
+{
+  return x * x + y * y;
 }
 
 template <class T>
-inline
-Vec2T<T> operator*(const T& s, const Vec2T<T>& a_other) noexcept {
-  return a_other*s;
+inline Vec2T<T>
+operator*(const T& s, const Vec2T<T>& a_other) noexcept
+{
+  return a_other * s;
 }
 
 template <class T>
-inline
-Vec2T<T> operator/(const T& s, const Vec2T<T>& a_other) noexcept {
-  return a_other/s;
+inline Vec2T<T>
+operator/(const T& s, const Vec2T<T>& a_other) noexcept
+{
+  return a_other / s;
 }
 
 template <class T>
-inline
-Vec3T<T>::Vec3T(){
+inline Vec3T<T>::Vec3T()
+{
   (*this) = Vec3T<T>::zero();
 }
 
 template <class T>
-inline
-Vec3T<T>::Vec3T(const Vec3T<T>& u) noexcept {
+inline Vec3T<T>::Vec3T(const Vec3T<T>& u) noexcept
+{
   X[0] = u[0];
   X[1] = u[1];
   X[2] = u[2];
 }
 
 template <class T>
-inline
-Vec3T<T>::Vec3T(const T& a_x, const T& a_y, const T& a_z){
+inline Vec3T<T>::Vec3T(const T& a_x, const T& a_y, const T& a_z)
+{
   X[0] = a_x;
   X[1] = a_y;
   X[2] = a_z;
 }
 
 template <class T>
-inline
-constexpr Vec3T<T> Vec3T<T>::zero() noexcept {
+inline constexpr Vec3T<T>
+Vec3T<T>::zero() noexcept
+{
   return Vec3T<T>(0, 0, 0);
 }
 
 template <class T>
-inline
-constexpr Vec3T<T> Vec3T<T>::one() noexcept {
+inline constexpr Vec3T<T>
+Vec3T<T>::one() noexcept
+{
   return Vec3T<T>(1, 1, 1);
 }
 
 template <class T>
-inline
-constexpr Vec3T<T> Vec3T<T>::unit(const size_t a_dir) noexcept {
+inline constexpr Vec3T<T>
+Vec3T<T>::unit(const size_t a_dir) noexcept
+{
   Vec3T<T> v = Vec3T<T>::zero();
   v[a_dir] = 1.0;
 
@@ -222,26 +245,31 @@ constexpr Vec3T<T> Vec3T<T>::unit(const size_t a_dir) noexcept {
 }
 
 template <class T>
-inline
-constexpr Vec3T<T> Vec3T<T>::min() noexcept {
+inline constexpr Vec3T<T>
+Vec3T<T>::min() noexcept
+{
   return Vec3T<T>(-std::numeric_limits<T>::max(), -std::numeric_limits<T>::max(), -std::numeric_limits<T>::max());
 }
 
 template <class T>
-inline
-constexpr Vec3T<T> Vec3T<T>::max() noexcept {
+inline constexpr Vec3T<T>
+Vec3T<T>::max() noexcept
+{
   return Vec3T<T>(std::numeric_limits<T>::max(), std::numeric_limits<T>::max(), std::numeric_limits<T>::max());
 }
 
 template <class T>
-inline
-constexpr Vec3T<T> Vec3T<T>::infinity() noexcept {
-  return Vec3T<T>(std::numeric_limits<T>::infinity(), std::numeric_limits<T>::infinity(), std::numeric_limits<T>::infinity());
+inline constexpr Vec3T<T>
+Vec3T<T>::infinity() noexcept
+{
+  return Vec3T<T>(
+    std::numeric_limits<T>::infinity(), std::numeric_limits<T>::infinity(), std::numeric_limits<T>::infinity());
 }
 
 template <class T>
-inline
-Vec3T<T>& Vec3T<T>::operator=(const Vec3T<T>& u) noexcept {
+inline Vec3T<T>&
+Vec3T<T>::operator=(const Vec3T<T>& u) noexcept
+{
   X[0] = u[0];
   X[1] = u[1];
   X[2] = u[2];
@@ -250,45 +278,52 @@ Vec3T<T>& Vec3T<T>::operator=(const Vec3T<T>& u) noexcept {
 }
 
 template <class T>
-inline
-Vec3T<T> Vec3T<T>::operator+(const Vec3T<T>& u) const noexcept {
-  return Vec3T<T>(X[0]+u[0], X[1]+u[1], X[2]+u[2]);
+inline Vec3T<T>
+Vec3T<T>::operator+(const Vec3T<T>& u) const noexcept
+{
+  return Vec3T<T>(X[0] + u[0], X[1] + u[1], X[2] + u[2]);
 }
 
 template <class T>
-inline
-Vec3T<T> Vec3T<T>::operator-(const Vec3T<T>& u) const noexcept {
-  return Vec3T<T>(X[0]-u[0], X[1]-u[1], X[2]-u[2]);
+inline Vec3T<T>
+Vec3T<T>::operator-(const Vec3T<T>& u) const noexcept
+{
+  return Vec3T<T>(X[0] - u[0], X[1] - u[1], X[2] - u[2]);
 }
 
 template <class T>
-inline
-Vec3T<T> Vec3T<T>::operator-() const noexcept {
+inline Vec3T<T>
+Vec3T<T>::operator-() const noexcept
+{
   return Vec3T<T>(-X[0], -X[1], -X[2]);
 }
 
 template <class T>
-inline
-Vec3T<T> Vec3T<T>::operator*(const T& s) const noexcept {
-  return Vec3T<T>(s*X[0], s*X[1], s*X[2]);
+inline Vec3T<T>
+Vec3T<T>::operator*(const T& s) const noexcept
+{
+  return Vec3T<T>(s * X[0], s * X[1], s * X[2]);
 }
 
 template <class T>
-inline
-Vec3T<T> Vec3T<T>::operator/(const T& s) const noexcept {
-  const T is = 1./s;
-  return Vec3T<T>(is*X[0], is*X[1], is*X[2]);
+inline Vec3T<T>
+Vec3T<T>::operator/(const T& s) const noexcept
+{
+  const T is = 1. / s;
+  return Vec3T<T>(is * X[0], is * X[1], is * X[2]);
 }
 
 template <class T>
-inline
-Vec3T<T> Vec3T<T>::operator/(const Vec3T<T>& v) const noexcept {
-  return Vec3T<T>(X[0]/v[0], X[1]/v[1], X[2]/v[2]);
+inline Vec3T<T>
+Vec3T<T>::operator/(const Vec3T<T>& v) const noexcept
+{
+  return Vec3T<T>(X[0] / v[0], X[1] / v[1], X[2] / v[2]);
 }
-  
+
 template <class T>
-inline
-Vec3T<T>& Vec3T<T>::operator+=(const Vec3T<T>& u) noexcept {
+inline Vec3T<T>&
+Vec3T<T>::operator+=(const Vec3T<T>& u) noexcept
+{
   X[0] += u[0];
   X[1] += u[1];
   X[2] += u[2];
@@ -297,8 +332,9 @@ Vec3T<T>& Vec3T<T>::operator+=(const Vec3T<T>& u) noexcept {
 }
 
 template <class T>
-inline
-Vec3T<T>& Vec3T<T>::operator-=(const Vec3T<T>& u) noexcept {
+inline Vec3T<T>&
+Vec3T<T>::operator-=(const Vec3T<T>& u) noexcept
+{
   X[0] -= u[0];
   X[1] -= u[1];
   X[2] -= u[2];
@@ -307,9 +343,10 @@ Vec3T<T>& Vec3T<T>::operator-=(const Vec3T<T>& u) noexcept {
 }
 
 template <class T>
-  
-inline
-Vec3T<T>& Vec3T<T>::operator*=(const T& s) noexcept {
+
+inline Vec3T<T>&
+Vec3T<T>::operator*=(const T& s) noexcept
+{
   X[0] *= s;
   X[1] *= s;
   X[2] *= s;
@@ -318,10 +355,11 @@ Vec3T<T>& Vec3T<T>::operator*=(const T& s) noexcept {
 }
 
 template <class T>
-inline
-Vec3T<T>& Vec3T<T>::operator/=(const T& s) noexcept {
-  const T is = 1./s;
-    
+inline Vec3T<T>&
+Vec3T<T>::operator/=(const T& s) noexcept
+{
+  const T is = 1. / s;
+
   X[0] *= is;
   X[1] *= is;
   X[2] *= is;
@@ -330,28 +368,30 @@ Vec3T<T>& Vec3T<T>::operator/=(const T& s) noexcept {
 }
 
 template <class T>
-inline
-Vec3T<T> Vec3T<T>::cross(const Vec3T<T>& u) const noexcept {
-  return Vec3T<T>(X[1]*u[2] - X[2]*u[1],
-		  X[2]*u[0] - X[0]*u[2],
-		  X[0]*u[1] - X[1]*u[0]);
+inline Vec3T<T>
+Vec3T<T>::cross(const Vec3T<T>& u) const noexcept
+{
+  return Vec3T<T>(X[1] * u[2] - X[2] * u[1], X[2] * u[0] - X[0] * u[2], X[0] * u[1] - X[1] * u[0]);
 }
 
 template <class T>
-inline
-T& Vec3T<T>::operator[](size_t i) noexcept {
+inline T&
+Vec3T<T>::operator[](size_t i) noexcept
+{
   return X[i];
 }
 
 template <class T>
-inline
-const T& Vec3T<T>::operator[](size_t i) const noexcept {
+inline const T&
+Vec3T<T>::operator[](size_t i) const noexcept
+{
   return X[i];
 }
 
 template <class T>
-inline
-Vec3T<T> Vec3T<T>::min(const Vec3T<T>& u) noexcept {
+inline Vec3T<T>
+Vec3T<T>::min(const Vec3T<T>& u) noexcept
+{
   X[0] = std::min(X[0], u[0]);
   X[1] = std::min(X[1], u[1]);
   X[2] = std::min(X[2], u[2]);
@@ -360,8 +400,9 @@ Vec3T<T> Vec3T<T>::min(const Vec3T<T>& u) noexcept {
 }
 
 template <class T>
-inline
-Vec3T<T> Vec3T<T>::max(const Vec3T<T>& u) noexcept {
+inline Vec3T<T>
+Vec3T<T>::max(const Vec3T<T>& u) noexcept
+{
   X[0] = std::max(X[0], u[0]);
   X[1] = std::max(X[1], u[1]);
   X[2] = std::max(X[2], u[2]);
@@ -370,159 +411,171 @@ Vec3T<T> Vec3T<T>::max(const Vec3T<T>& u) noexcept {
 }
 
 template <class T>
-inline
-size_t Vec3T<T>::minDir(const bool a_doAbs) const noexcept {
+inline size_t
+Vec3T<T>::minDir(const bool a_doAbs) const noexcept
+{
   size_t mDir = 0;
-    
-  for (size_t dir = 0; dir < 3; dir++){
-    if (a_doAbs){
+
+  for (size_t dir = 0; dir < 3; dir++) {
+    if (a_doAbs) {
       if (std::abs(X[dir]) < std::abs(X[mDir])) {
-	mDir = dir;
+        mDir = dir;
       }
-    }
-    else{
-      if (X[dir] < X[mDir]){
-	mDir = dir;
+    } else {
+      if (X[dir] < X[mDir]) {
+        mDir = dir;
       }
     }
   }
-    
+
   return mDir;
 }
 
 template <class T>
-inline
-size_t Vec3T<T>::maxDir(const bool a_doAbs) const noexcept {
+inline size_t
+Vec3T<T>::maxDir(const bool a_doAbs) const noexcept
+{
   size_t mDir = 0;
-    
-  for (size_t dir = 0; dir < 3; dir++){
-    if (a_doAbs){
+
+  for (size_t dir = 0; dir < 3; dir++) {
+    if (a_doAbs) {
       if (std::abs(X[dir]) > std::abs(X[mDir])) {
-	mDir = dir;
+        mDir = dir;
       }
-    }
-    else{
-      if (X[dir] > X[mDir]){
-	mDir = dir;
+    } else {
+      if (X[dir] > X[mDir]) {
+        mDir = dir;
       }
     }
   }
-    
+
   return mDir;
 }
 
 template <class T>
-inline
-bool Vec3T<T>::operator==(const Vec3T<T>& u) const noexcept {
-  return (X[0]==u[0] && X[1]==u[1] && X[2]==u[2]);
+inline bool
+Vec3T<T>::operator==(const Vec3T<T>& u) const noexcept
+{
+  return (X[0] == u[0] && X[1] == u[1] && X[2] == u[2]);
 }
 
 template <class T>
-inline
-bool Vec3T<T>::operator<(const Vec3T<T>& u) const noexcept {
-  return (X[0]<u[0] && X[1]<u[1] && X[2]<u[2]);
+inline bool
+Vec3T<T>::operator<(const Vec3T<T>& u) const noexcept
+{
+  return (X[0] < u[0] && X[1] < u[1] && X[2] < u[2]);
 }
 
 template <class T>
-inline
-bool Vec3T<T>::operator>(const Vec3T<T>& u) const noexcept {
-  return (X[0]>u[0] && X[1]>u[1] && X[2]>u[2]);
+inline bool
+Vec3T<T>::operator>(const Vec3T<T>& u) const noexcept
+{
+  return (X[0] > u[0] && X[1] > u[1] && X[2] > u[2]);
 }
 
 template <class T>
-inline
-bool Vec3T<T>::operator<=(const Vec3T<T>& u) const noexcept {
-  return (X[0]<=u[0] && X[1]<=u[1] && X[2]<=u[2]);
+inline bool
+Vec3T<T>::operator<=(const Vec3T<T>& u) const noexcept
+{
+  return (X[0] <= u[0] && X[1] <= u[1] && X[2] <= u[2]);
 }
 
 template <class T>
-  
-inline
-bool Vec3T<T>::operator>=(const Vec3T<T>& u) const noexcept {
-  return (X[0]>=u[0] && X[1]>=u[1] && X[2]>=u[2]);
+
+inline bool
+Vec3T<T>::operator>=(const Vec3T<T>& u) const noexcept
+{
+  return (X[0] >= u[0] && X[1] >= u[1] && X[2] >= u[2]);
 }
 
 template <class T>
-inline
-T Vec3T<T>::dot(const Vec3T<T>& u) const noexcept {
-  return X[0]*u[0] + X[1]*u[1] + X[2]*u[2];
+inline T
+Vec3T<T>::dot(const Vec3T<T>& u) const noexcept
+{
+  return X[0] * u[0] + X[1] * u[1] + X[2] * u[2];
 }
 
 template <class T>
-inline
-T Vec3T<T>::length() const noexcept {
-  return sqrt(X[0]*X[0] + X[1]*X[1] + X[2]*X[2]);
+inline T
+Vec3T<T>::length() const noexcept
+{
+  return sqrt(X[0] * X[0] + X[1] * X[1] + X[2] * X[2]);
 }
 
 template <class T>
-inline
-T Vec3T<T>::length2() const noexcept {
-  return X[0]*X[0] + X[1]*X[1] + X[2]*X[2];
+inline T
+Vec3T<T>::length2() const noexcept
+{
+  return X[0] * X[0] + X[1] * X[1] + X[2] * X[2];
 }
 
 template <class T>
-inline
-Vec3T<T> operator*(const T& s, const Vec3T<T>& a_other) noexcept {
-  return a_other*s;
+inline Vec3T<T>
+operator*(const T& s, const Vec3T<T>& a_other) noexcept
+{
+  return a_other * s;
 }
 
 template <class T>
-inline
-Vec3T<T> operator/(const T& s, const Vec3T<T>& a_other) noexcept {
-  return a_other/s;
+inline Vec3T<T>
+operator/(const T& s, const Vec3T<T>& a_other) noexcept
+{
+  return a_other / s;
 }
 
 template <class T>
-inline
-Vec3T<T> min(const Vec3T<T>& u, const Vec3T<T>& v) noexcept {
-  return Vec3T<T>(std::min(u[0], v[0]),
-		  std::min(u[1], v[1]),
-		  std::min(u[2], v[2]));
+inline Vec3T<T>
+min(const Vec3T<T>& u, const Vec3T<T>& v) noexcept
+{
+  return Vec3T<T>(std::min(u[0], v[0]), std::min(u[1], v[1]), std::min(u[2], v[2]));
 }
 
 template <class T>
-inline
-Vec3T<T> max(const Vec3T<T>& u, const Vec3T<T>& v) noexcept {
-  return Vec3T<T>(std::max(u[0], v[0]),
-		  std::max(u[1], v[1]),
-		  std::max(u[2], v[2]));
+inline Vec3T<T>
+max(const Vec3T<T>& u, const Vec3T<T>& v) noexcept
+{
+  return Vec3T<T>(std::max(u[0], v[0]), std::max(u[1], v[1]), std::max(u[2], v[2]));
 }
 
 template <class T>
-inline
-T dot(const Vec3T<T>& u, const Vec3T<T>& v) noexcept {
+inline T
+dot(const Vec3T<T>& u, const Vec3T<T>& v) noexcept
+{
   return u.dot(v);
 }
 
 template <class T>
-inline
-T length(const Vec3T<T>& v) noexcept {
+inline T
+length(const Vec3T<T>& v) noexcept
+{
   return v.length();
 }
 
 template <class T>
-inline
-Vec2T<T> min(const Vec2T<T>& u, const Vec2T<T>& v) noexcept {
-  return Vec2T<T>(std::min(u.x, v.x),
-		  std::min(u.y, v.y));
+inline Vec2T<T>
+min(const Vec2T<T>& u, const Vec2T<T>& v) noexcept
+{
+  return Vec2T<T>(std::min(u.x, v.x), std::min(u.y, v.y));
 }
 
 template <class T>
-inline
-Vec2T<T> max(const Vec2T<T>& u, const Vec2T<T>& v) noexcept {
-  return Vec2T<T>(std::max(u.x, v.x),
-		  std::max(u.y, v.y));
+inline Vec2T<T>
+max(const Vec2T<T>& u, const Vec2T<T>& v) noexcept
+{
+  return Vec2T<T>(std::max(u.x, v.x), std::max(u.y, v.y));
 }
 
 template <class T>
-inline
-T dot(const Vec2T<T>& u, const Vec2T<T>& v) noexcept {
+inline T
+dot(const Vec2T<T>& u, const Vec2T<T>& v) noexcept
+{
   return u.dot(v);
 }
 
 template <class T>
-inline
-T length(const Vec2T<T>& v) noexcept {
+inline T
+length(const Vec2T<T>& v) noexcept
+{
   return v.length();
 }
 
