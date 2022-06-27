51d667f6dc0076d884d504cfde7e3fd1264ae4c2
Robert Marskar <robert.marskar@sintef.no>
2022-06-27 15:11:20 +0200

    Update main-file

--- Examples/EBGeometry_F18/main.cpp
+++ Examples/EBGeometry_F18/main.cpp
@@ -11,7 +11,6 @@
 
 #include "../../EBGeometry.hpp"
 
-namespace fs = std::filesystem;
 using namespace EBGeometry;
 using namespace EBGeometry::DCEL;
 
@@ -20,6 +19,7 @@ using T = float;
 using Face = EBGeometry::DCEL::FaceT<T>;
 using Mesh = EBGeometry::DCEL::MeshT<T>;
 using BV   = EBGeometry::BoundingVolumes::AABBT<T>;
+using Prim = EBGeometry::BVH::LinearBVH<T, Face, BV, K>;
 
 int
 main(int argc, char* argv[])
@@ -27,7 +27,7 @@ main(int argc, char* argv[])
 
   // Get all the STL files in this directory . 
   std::vector<std::string> stlFiles;
-  for (const auto & entry : fs::directory_iterator("./")) {
+  for (const auto & entry : std::filesystem::directory_iterator("./")) {
     const std::string f   = entry.path();
     const std::string ext = f.substr(f.find_last_of(".") + 1);
 
@@ -36,10 +36,14 @@ main(int argc, char* argv[])
     }
   }
 
-  // Read each STL file and make them into flattened BVH-trees. 
+  // Read each STL file and make them into flattened BVH-trees. These STL files
+  // have the normal vector pointing inwards so we flip it. 
   std::vector<std::shared_ptr<EBGeometry::BVH::LinearBVH<T, Face, BV, K>>> bvhDCEL;
   for (const auto& f : stlFiles) {
     const auto mesh = EBGeometry::Parser::read<T>(f);
+    mesh->flip();
+
+    // Create the BVH. 
     auto bvh = std::make_shared<EBGeometry::BVH::NodeT<T, Face, BV, K>>(mesh->getFaces());
     bvh->topDownSortAndPartitionPrimitives(EBGeometry::DCEL::defaultBVConstructor<T, BV>,
 					   EBGeometry::DCEL::defaultPartitioner<T, BV, K>,
@@ -47,20 +51,18 @@ main(int argc, char* argv[])
     bvhDCEL.emplace_back(bvh->flattenTree());
   }
 
-  // Cast those fuckers.
-  std::vector<std::shared_ptr<EBGeometry::SignedDistanceFunction<T> > > sdfs;
-  for (const auto& cur : bvhDCEL) {
-    sdfs.emplace_back(cur);
-  }
+  // Standard, slower union which iterates through every BVH. 
+  EBGeometry::Union<Prim, T> slowUnion(bvhDCEL, false);  
 
-  // Make a BVH-of-BVH union
-  using Prim = EBGeometry::BVH::LinearBVH<T, Face, BV, K>;
+  // Optimized, faster union which uses a BVH for bounding the BVHs. 
   EBGeometry::BVH::BVConstructorT<Prim, BV> bvConstructor = [](const std::shared_ptr<const Prim>& a_prim) -> BV {
     return a_prim->getBoundingVolume();
   };
+  EBGeometry::UnionBVH<T, Prim, BV, K> fastUnion(bvhDCEL, false, bvConstructor);
 
-  EBGeometry::UnionBVH<T, BV, K> implicitFunction(bvhDCEL, false, bvConstructor);
-
+  // Compute the value function. 
+  std::cout << "fast value = " <<  fastUnion.value(EBGeometry::Vec3T<T>::one()) << "\n";
+  std::cout << "slow value = " <<  slowUnion.value(EBGeometry::Vec3T<T>::one()) << "\n";  
   
   return 0;
 }
