65caa292e91692a3ee0bb52f3ac596feedf2d26d
Robert Marskar <robert.marskar@sintef.no>
2022-06-27 15:52:37 +0200

    Start on F18 example

---
+++ Examples/AMReX_F18/main.cpp
@@ -0,0 +1,142 @@
+/* EBGeometry
+ * Copyright © 2022 Robert Marskar
+ * Please refer to Copyright.txt and LICENSE in the EBGeometry root directory.
+ */
+
+// AMReX includes
+#include <AMReX.H>
+#include <AMReX_EB2.H>
+#include <AMReX_EB2_IF.H>
+#include <AMReX_ParmParse.H>
+#include <AMReX_PlotFileUtil.H>
+#include <filesystem>
+
+// Our include
+#include "../../EBGeometry.hpp"
+
+using namespace amrex;
+
+constexpr size_t K = 4;
+using T            = float;
+using Face         = EBGeometry::DCEL::FaceT<T>;
+using Mesh         = EBGeometry::DCEL::MeshT<T>;
+using BV           = EBGeometry::BoundingVolumes::AABBT<T>;
+using Prim         = EBGeometry::BVH::LinearBVH<T, Face, BV, K>;
+using Vec3         = EBGeometry::Vec3T<T>;
+
+namespace amrex {
+  namespace EB2 {
+
+    // F18 geometry, using nifty EBGeometry bindings and accelerators. 
+    class F18
+    {
+    public:
+      F18()
+      {
+	// Get all the STL files in this directory .
+	std::vector<std::string> stlFiles;
+	for (const auto& entry : std::filesystem::directory_iterator("../Objects/F18")) {
+	  const std::string f   = entry.path();
+	  const std::string ext = f.substr(f.find_last_of(".") + 1);
+
+	  if (ext == "stl" || ext == "STL") {
+	    stlFiles.emplace_back(f);
+	  }
+	}
+
+	// // Read each STL file and make them into flattened BVH-trees. These STL files
+	// // have the normal vector pointing inwards so we flip it.
+	// std::vector<std::shared_ptr<EBGeometry::BVH::LinearBVH<T, Face, BV, K>>> bvhDCEL;
+	// for (const auto& f : stlFiles) {
+	//   const auto mesh = EBGeometry::Parser::read<T>(f);
+	//   mesh->flip();
+
+	//   // Create the BVH.
+	//   auto bvh = std::make_shared<EBGeometry::BVH::NodeT<T, Face, BV, K>>(mesh->getFaces());
+	//   bvh->topDownSortAndPartitionPrimitives(EBGeometry::DCEL::defaultBVConstructor<T, BV>,
+	// 					 EBGeometry::DCEL::defaultPartitioner<T, BV, K>,
+	// 					 EBGeometry::DCEL::defaultStopFunction<T, BV, K>);
+	//   bvhDCEL.emplace_back(bvh->flattenTree());
+	// }
+
+	// // Optimized, faster union which uses a BVH for bounding the BVHs.
+	// EBGeometry::BVH::BVConstructorT<Prim, BV> bvConstructor = [](const std::shared_ptr<const Prim>& a_prim) -> BV {
+	//   return a_prim->getBoundingVolume();
+	// };
+
+	// m_union = std::make_shared<EBGeometry::UnionBVH<T, Prim, BV, K>>(bvhDCEL, true, bvConstructor);
+      }
+
+      F18(const F18& a_other)
+      {
+        this->m_union = a_other.m_union;
+      }
+
+      Real operator()(AMREX_D_DECL(Real x, Real y, Real z)) const noexcept
+      {
+        return m_union->value(Vec3(x,y,z));
+      };
+
+      inline Real
+      operator()(const RealArray& p) const noexcept
+      {
+        return this->operator()(AMREX_D_DECL(p[0], p[1], p[2]));
+      }
+
+    protected:
+      
+      std::shared_ptr<EBGeometry::UnionBVH<T, Prim, BV, K> > m_union;
+    };
+  } // namespace EB2
+} // namespace amrex
+
+int
+main(int argc, char* argv[])
+{
+  amrex::Initialize(argc, argv);
+
+  int n_cell        = 128;
+  int max_grid_size = 32;
+  int which_geom    = 0;
+
+  std::string filename;
+
+  // read parameters
+  ParmParse pp;
+  pp.query("n_cell", n_cell);
+  pp.query("max_grid_size", max_grid_size);
+  pp.query("which_geom", which_geom);
+
+  Geometry geom;
+  {
+    RealBox rb = RealBox({-10, -10, -10}, {10, 10, 10});
+
+    Array<int, AMREX_SPACEDIM> is_periodic{false, false, false};
+    Geometry::Setup(&rb, 0, is_periodic.data());
+    Box domain(IntVect(0), IntVect(n_cell - 1));
+    geom.define(domain);
+  }
+
+  EB2::F18 f18();
+
+  auto gshop = EB2::makeShop(f18);
+  EB2::Build(gshop, geom, 0, 0);
+
+  // Put some data
+  MultiFab mf;
+  {
+    BoxArray boxArray(geom.Domain());
+    boxArray.maxSize(max_grid_size);
+    DistributionMapping dm{boxArray};
+
+    std::unique_ptr<EBFArrayBoxFactory> factory =
+      amrex::makeEBFabFactory(geom, boxArray, dm, {2, 2, 2}, EBSupport::full);
+
+    mf.define(boxArray, dm, 1, 0, MFInfo(), *factory);
+    mf.setVal(1.0);
+  }
+
+  EB_WriteSingleLevelPlotfile("plt", mf, {"rho"}, geom, 0.0, 0);
+
+  amrex::Finalize();
+}
