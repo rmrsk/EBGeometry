2d58e20ea2170e86fdb7100f44c5b9bde716c2c6
rmrsk <robert.marskar@gmail.com>
2023-09-29 18:24:05 +0000

    Deploying to gh-pages from  @ 2666a6682237077396af1902db2b8c6bdc96e8e5 🚀

--- doxygen/html/search/all_d.js
+++ doxygen/html/search/all_d.js
@@ -1,17 +1,17 @@
 var searchData=
 [
-  ['parser_266',['Parser',['../namespaceParser.html',1,'']]],
-  ['partitioner_267',['Partitioner',['../classBVH_1_1NodeT.html#ab3b7afb83f7ecf97d1985c3c5ade2f0a',1,'BVH::NodeT']]],
-  ['partitionert_268',['PartitionerT',['../namespaceBVH.html#ae7e5e97e86dfe5a7103cbf2918f0cfc9',1,'BVH']]],
-  ['planesdf_269',['PlaneSDF',['../classPlaneSDF.html#acbd51b3dace74b7fd8bc94ce6c8715c2',1,'PlaneSDF::PlaneSDF()=delete'],['../classPlaneSDF.html#a698edd3858784ec37d51de52202fad02',1,'PlaneSDF::PlaneSDF(const Vec3T&lt; T &gt; &amp;a_point, const Vec3T&lt; T &gt; &amp;a_normal)'],['../classPlaneSDF.html',1,'PlaneSDF&lt; T &gt;']]],
-  ['ply_270',['PLY',['../classParser_1_1PLY.html',1,'Parser']]],
-  ['polygon2d_271',['Polygon2D',['../classPolygon2D.html',1,'Polygon2D&lt; T &gt;'],['../classPolygon2D.html#a9733800001f7eba9faebcd86617699ae',1,'Polygon2D::Polygon2D()=delete'],['../classPolygon2D.html#adf4ae9b914197276beb7cc3925ad24f0',1,'Polygon2D::Polygon2D(const Vec3 &amp;a_normal, const std::vector&lt; Vec3 &gt; &amp;a_points)']]],
-  ['primandbv_272',['PrimAndBV',['../namespaceBVH.html#a2f1baf3f4361b542e8b582b2d64954ce',1,'BVH']]],
-  ['primandbvlistt_273',['PrimAndBVListT',['../namespaceBVH.html#a1edeb7c10026d6fba3e35536a6719a3a',1,'BVH']]],
-  ['primitivecentroidpartitioner_274',['PrimitiveCentroidPartitioner',['../namespaceBVH.html#aa7c7fa51a4c99cebf11a6261b6be2b8a',1,'BVH']]],
-  ['primitivelist_275',['PrimitiveList',['../classBVH_1_1NodeT.html#a19cce6e7fbe85eccb4a3718dd69f49b7',1,'BVH::NodeT::PrimitiveList()'],['../classBVH_1_1LinearBVH.html#a94ee5da1670e2ef85eeabf7cf6a2da92',1,'BVH::LinearBVH::PrimitiveList()']]],
-  ['primitivelistt_276',['PrimitiveListT',['../namespaceBVH.html#aa1e753bda451b85cd5b948722a2ad7c7',1,'BVH']]],
-  ['printwarnings_277',['printWarnings',['../classDCEL_1_1MeshT.html#aee2a8a6a821545c11fa0c67381b6abd1',1,'DCEL::MeshT']]],
-  ['projectpointintofaceplane_278',['projectPointIntoFacePlane',['../classDCEL_1_1FaceT.html#ae0270906ecae58fd80f30929b4e0fd79',1,'DCEL::FaceT']]],
-  ['projectpointtoedge_279',['projectPointToEdge',['../classDCEL_1_1EdgeT.html#ad3d93918b8ccbbdfba50d74107f162d7',1,'DCEL::EdgeT']]]
+  ['parser_267',['Parser',['../namespaceParser.html',1,'']]],
+  ['partitioner_268',['Partitioner',['../classBVH_1_1NodeT.html#ab3b7afb83f7ecf97d1985c3c5ade2f0a',1,'BVH::NodeT']]],
+  ['partitionert_269',['PartitionerT',['../namespaceBVH.html#ae7e5e97e86dfe5a7103cbf2918f0cfc9',1,'BVH']]],
+  ['planesdf_270',['PlaneSDF',['../classPlaneSDF.html#acbd51b3dace74b7fd8bc94ce6c8715c2',1,'PlaneSDF::PlaneSDF()=delete'],['../classPlaneSDF.html#a698edd3858784ec37d51de52202fad02',1,'PlaneSDF::PlaneSDF(const Vec3T&lt; T &gt; &amp;a_point, const Vec3T&lt; T &gt; &amp;a_normal)'],['../classPlaneSDF.html',1,'PlaneSDF&lt; T &gt;']]],
+  ['ply_271',['PLY',['../classParser_1_1PLY.html',1,'Parser']]],
+  ['polygon2d_272',['Polygon2D',['../classPolygon2D.html',1,'Polygon2D&lt; T &gt;'],['../classPolygon2D.html#a9733800001f7eba9faebcd86617699ae',1,'Polygon2D::Polygon2D()=delete'],['../classPolygon2D.html#adf4ae9b914197276beb7cc3925ad24f0',1,'Polygon2D::Polygon2D(const Vec3 &amp;a_normal, const std::vector&lt; Vec3 &gt; &amp;a_points)']]],
+  ['primandbv_273',['PrimAndBV',['../namespaceBVH.html#a2f1baf3f4361b542e8b582b2d64954ce',1,'BVH']]],
+  ['primandbvlistt_274',['PrimAndBVListT',['../namespaceBVH.html#a1edeb7c10026d6fba3e35536a6719a3a',1,'BVH']]],
+  ['primitivecentroidpartitioner_275',['PrimitiveCentroidPartitioner',['../namespaceBVH.html#aa7c7fa51a4c99cebf11a6261b6be2b8a',1,'BVH']]],
+  ['primitivelist_276',['PrimitiveList',['../classBVH_1_1NodeT.html#a19cce6e7fbe85eccb4a3718dd69f49b7',1,'BVH::NodeT::PrimitiveList()'],['../classBVH_1_1LinearBVH.html#a94ee5da1670e2ef85eeabf7cf6a2da92',1,'BVH::LinearBVH::PrimitiveList()']]],
+  ['primitivelistt_277',['PrimitiveListT',['../namespaceBVH.html#aa1e753bda451b85cd5b948722a2ad7c7',1,'BVH']]],
+  ['printwarnings_278',['printWarnings',['../classDCEL_1_1MeshT.html#aee2a8a6a821545c11fa0c67381b6abd1',1,'DCEL::MeshT']]],
+  ['projectpointintofaceplane_279',['projectPointIntoFacePlane',['../classDCEL_1_1FaceT.html#ae0270906ecae58fd80f30929b4e0fd79',1,'DCEL::FaceT']]],
+  ['projectpointtoedge_280',['projectPointToEdge',['../classDCEL_1_1EdgeT.html#ad3d93918b8ccbbdfba50d74107f162d7',1,'DCEL::EdgeT']]]
 ];
--- doxygen/html/search/variables_7.js
+++ doxygen/html/search/variables_7.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['smoothmax_806',['smoothMax',['../EBGeometry__CSG_8hpp.html#ae88258939353042593b6ab1401d3bc46',1,'EBGeometry_CSG.hpp']]],
-  ['smoothmin_807',['smoothMin',['../EBGeometry__CSG_8hpp.html#aa6a6befc82e122b0943c4653290b3259',1,'EBGeometry_CSG.hpp']]]
+  ['smoothmax_807',['smoothMax',['../EBGeometry__CSG_8hpp.html#ae88258939353042593b6ab1401d3bc46',1,'EBGeometry_CSG.hpp']]],
+  ['smoothmin_808',['smoothMin',['../EBGeometry__CSG_8hpp.html#aa6a6befc82e122b0943c4653290b3259',1,'EBGeometry_CSG.hpp']]]
 ];
