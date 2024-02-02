7665aed530cc4402bb9eb76632d8dc3c24192393
Robert Marskar <robert.marskar@gmail.com>
2024-02-02 09:55:14 +0100

    Add EB-painting example as development guide

---
+++ Examples/AMReX_PaintEB/README.md
@@ -0,0 +1,41 @@
+Examples/AMReX_DCEL
+-------------------
+
+This example uses the embedded boundary grid generation from AMReX.
+To compile this application, first install AMReX somewhere and point the AMREX_HOME environment variable to it.
+
+Compiling
+---------
+
+Compile (with your standard AMReX settings) using
+
+    make -s -j8 DIM=3
+
+Running
+-------
+
+With MPI:
+
+    mpirun -np 8 main3d.<something>.ex eb2.cover_multiple_cuts=1 which_geom=0
+
+Some of the geometries will generate cut-cells which AMReX does not support, so the geometries should be run with eb2.cover_multiple_cuts=1.
+
+Other input options are
+
+* `use_bvh = true/false` Turn on/off the BVH accelerator. 
+* `n_cell = <integer>` For setting the number of grid cells along the coordinate directions.
+* `max_grid_size = <integer>` For setting the blocking factor.
+* `num_coarsen_opt = <integer>` For performance tuning the EB generation.
+
+Supported geometries
+--------------------
+To select different geometries, set which_geom to one of the below.
+
+* `which_geom = 0` Airfoil
+* `which_geom = 1` Sphere
+* `which_geom = 2` Dodecahedron
+* `which_geom = 3` Horse
+* `which_geom = 4` Porsche
+* `which_geom = 5` Orion capsule
+* `which_geom = 6` Armadillo
+* `which_geom = 7` Adirondack
---
+++ Examples/AMReX_PaintEB/main.cpp
@@ -0,0 +1,174 @@
+/* EBGeometry
+ * Copyright © 2022 Robert Marskar
+ * Please refer to Copyright.txt and LICENSE in the EBGeometry root directory.
+ */
+
+// AMReX includes
+#include <AMReX.H>
+#include <AMReX_EB2.H>
+#include <AMReX_EB2_IF.H>
+#include <AMReX_ParmParse.H>
+#include <AMReX_PlotFileUtil.H>
+
+// Our include
+#include "../../EBGeometry.hpp"
+
+using namespace amrex;
+
+/*!
+  @brief This is an AMReX-capable version of the EBGeometry BVH accelerator. It
+  is templated as T, Meta BV, K which indicate the EBGeometry precision, triangle meta-data, 
+  bounding volume, and tree degree.
+*/
+template <class T, class BV, size_t K, class Meta = int>
+class AMReXSDF
+{
+public:
+  /*!
+    @brief Full constructor.
+    @param[in] a_filename File name. Must be an STL file.
+  */
+  AMReXSDF(const std::string a_filename)
+  {
+    m_mesh = EBGeometry::Parser::readIntoDCEL<T, Meta>(a_filename);
+    m_sdf  = std::make_shared<EBGeometry::FastMeshSDF<T, Meta, BV, K>>(m_mesh);
+  }
+
+  /*!
+    @brief Copy constructor.
+    @param[in] a_other Other SDF.
+  */
+  AMReXSDF(const AMReXSDF& a_other)
+  {
+    this->m_mesh = a_other.m_mesh;
+    this->m_sdf  = a_other.m_sdf;
+  }
+
+  /*!
+    @brief AMReX's implicit function definition.
+  */
+  Real operator()(AMREX_D_DECL(Real x, Real y, Real z)) const noexcept
+  {
+    using Vec3 = EBGeometry::Vec3T<T>;
+
+    return m_sdf->value(Vec3(x, y, z));
+  };
+
+  /*!
+    @brief Also an AMReX implicit function implementation
+  */
+  inline Real
+  operator()(const RealArray& p) const noexcept
+  {
+    return this->operator()(AMREX_D_DECL(p[0], p[1], p[2]));
+  }
+
+protected:
+  /*!
+    @brief Full DCEL mesh containing all the triangles. 
+  */
+  std::shared_ptr<EBGeometry::DCEL::MeshT<T, Meta>> m_mesh;
+
+  /*!
+    @brief DCEL mesh represented as a BVH of its facets, exposed as an implicit function. 
+  */
+  std::shared_ptr<EBGeometry::ImplicitFunction<T>> m_sdf;
+};
+
+int
+main(int argc, char* argv[])
+{
+
+  amrex::Initialize(argc, argv);
+
+  {
+    int n_cell          = 128;
+    int max_grid_size   = 16;
+    int which_geom      = 0;
+    int num_coarsen_opt = 0;
+
+    std::string filename;
+
+    // read parameters
+    ParmParse pp;
+    pp.query("n_cell", n_cell);
+    pp.query("max_grid_size", max_grid_size);
+    pp.query("which_geom", which_geom);
+    pp.query("num_coarsen_opt", num_coarsen_opt);
+
+    Geometry geom;
+    {
+      RealBox rb;
+
+      if (which_geom == 0) { // Airfoil case
+        rb       = RealBox({-100, -100, -75}, {400, 100, 125});
+        filename = "../Resources/airfoil.stl";
+      }
+      else if (which_geom == 1) { // Sphere case
+        rb       = RealBox({-400, -400, -400}, {400, 400, 400});
+        filename = "../Resources/sphere.stl";
+      }
+      else if (which_geom == 2) { // Dodecahedron
+        rb       = RealBox({-2., -2., -2.}, {2., 2., 2.});
+        filename = "../Resources/dodecahedron.stl";
+      }
+      else if (which_geom == 3) { // Horse
+        rb       = RealBox({-0.12, -0.12, -0.12}, {0.12, 0.12, 0.12});
+        filename = "../Resources/horse.stl";
+      }
+      else if (which_geom == 4) { // Car
+        //	    rb = RealBox({-20,-20,-20}, {20,20,20}); // Doesn't work.
+        rb       = RealBox({-10, -5, -5}, {10, 5, 5}); // Works.
+        filename = "../Resources/porsche.stl";
+      }
+      else if (which_geom == 5) { // Orion
+        rb       = RealBox({-10, -5, -10}, {10, 10, 10});
+        filename = "../Resources/orion.stl";
+      }
+      else if (which_geom == 6) { // Armadillo
+        rb       = RealBox({-100, -75, -100}, {100, 125, 100});
+        filename = "../Resources/armadillo.stl";
+      }
+      else if (which_geom == 7) { // Adirondacks
+        rb       = RealBox({0, 0, 0}, {200, 200, 50});
+        filename = "../Resources/adirondack.stl";
+      }
+
+      Array<int, AMREX_SPACEDIM> is_periodic{false, false, false};
+      Geometry::Setup(&rb, 0, is_periodic.data());
+      Box domain(IntVect(0), IntVect(n_cell - 1));
+      geom.define(domain);
+    }
+
+    // Create our signed distance function. K is the tree degree while T is the
+    // EBGeometry precision.
+    constexpr int K = 4;
+
+    using T    = float;
+    using Vec3 = EBGeometry::Vec3T<T>;
+    using BV   = EBGeometry::BoundingVolumes::AABBT<T>;
+
+    AMReXSDF<T, BV, K> sdf(filename);
+
+    auto gshop = EB2::makeShop(sdf);
+    EB2::Build(gshop, geom, 0, 0, 1, true, true, num_coarsen_opt);
+
+    // Put some data
+    MultiFab mf;
+    {
+      BoxArray boxArray(geom.Domain());
+      boxArray.maxSize(max_grid_size);
+      DistributionMapping dm{boxArray};
+
+      std::unique_ptr<EBFArrayBoxFactory> factory =
+        amrex::makeEBFabFactory(geom, boxArray, dm, {2, 2, 2}, EBSupport::full);
+
+      mf.define(boxArray, dm, 1, 0, MFInfo(), *factory);
+      mf.setVal(1.0);
+    }
+
+    EB_WriteSingleLevelPlotfile("plt", mf, {"rho"}, geom, 0.0, 0);
+  }
+
+  amrex::Finalize();
+}
